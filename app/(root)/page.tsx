1abfddd8faf43b37a7228ff05753bb7d6527677b
Aditya <adityaparghi26@gmail.com>
2025-02-17 13:03:08 +0530

    implement auth , appwrite and sentry

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,11 @@
 import HeaderBox from '@/components/HeaderBox'
 import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
+import { getLoggedInUser } from '@/lib/actions/user.actions';
 import React from 'react'
 
-const Home = () => {
-  const loggedIn = {firstName : 'Aditya',lastName:'Parghi',email:'adityaparghi26@gmail.com'};
+const Home = async() => {
+  const loggedIn =  await getLoggedInUser();   //{firstName : 'Aditya',lastName:'Parghi',email:'adityaparghi26@gmail.com'};
 
   return (
  <section className='home'> 
@@ -13,7 +14,7 @@ const Home = () => {
         <HeaderBox 
         type="greeting"
         title="Welcome"
-        user={loggedIn ?.firstName || 'Guest'}
+        user={loggedIn ?.name || 'Guest'}
         subtext = "Access your account and transactions efficiently."
         />
 
--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -14,6 +14,7 @@ import Link from 'next/link'
 import { sidebarLinks } from '@/constants'
 import { cn } from '@/lib/utils'
 import { usePathname } from 'next/navigation'
+import Footer from './Footer'
 
 
 const MobileNav = ({ user }: MobileNavProps) => {
@@ -70,7 +71,7 @@ const MobileNav = ({ user }: MobileNavProps) => {
                         </SheetClose>
                     </div>
 
-                    FOOTER
+                   <Footer user={user} type="mobile" />
                 </SheetContent>
             </Sheet>
 
